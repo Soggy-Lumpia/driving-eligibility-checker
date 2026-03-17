98567188d4a9457a89d1f31c143bf64d84b9cd3e
Soggy-Lumpia <145707058+Soggy-Lumpia@users.noreply.github.com>
2026-03-17 14:33:08 -0700

    Loops input x2 times

--- main.py
+++ main.py
@@ -2,104 +2,122 @@ import colorama
 
 
 def askUserAge() -> int:
-    getAge = int(input("What is your age?:"))    # Get variable data type -- DEBUG   
-    print(f"getAge data type: {type(getAge)}\n")
+    getAge = int(input("What is your age?:"))    # Get variable data type -- DEBUG
+    
+    # DEBUG
+    # print(f"getAge data type: {type(getAge)}\n")
+    
     # initialization
     agePass = getAge
-    
+    invalid_Input = False
+
     # First successful User-Case
     if agePass == int(getAge):
         getAgeDataType = type(getAge)
         print(f"getAge == int(getAge) conditional: \n{getAgeDataType}\n")
-        return getAge
-        
-        
-    return agePass
-        
+        return agePass
+    
+    # Check for STRING input 
+    elif agePass == isalpha(getAge):
+        # Switch
+        invalid_Input = True
+        # Checking for STRING --DEBUG
+        print("The input is a string!\n")
 
-def checkUserAge(age) -> bool: 
-        
+        # Repeat asking for
+        while(invalid_Input == True):
+            agePass = int(input("What is your age?:"))
+            break;
+            if(invalid_Input == True and agePass == isalpha(age)):
+                continue;
+                
+
+    return agePass
+
+
+def checkUserAge(age) -> bool:
+
         age = askUserAge()
         validAge = True
         invalidAge = False
-        
+
         while age >= 18:
-            
+
             # Ask User if they want a valid driver's license
             askDL = input("Do you want to apply for a driver's license?\n Y/N?\n")
             break;
-            
+
             if askDL == 'Y' or  askDL == 'y' or askDL == 'Yes' or askDL == "yes" or askDL == "yEs" or askDL == "yeS" or askDL == "yES" or askDL == "YES":
                 print("Yes condition!\n")
                 break;
-                
+
             elif askDL == 'N' or askDL == 'n' or askDL == "No" or askDL == "no" or askDL == "nO" or askDL == "NO":
                 print("No conditon!\n")
                 break;
             else:
                 print("This is a valid age!\n")
                 return validAge
-                
+
         # emulate do-while loop
         while age < 18:
             invalidAge = False
             if (age < 18 and invalidAge == False ):
                 print(f"You need to be 18 years or older to apply!\n")
                 break;
-                
+
             else:
                 print(f"{invalidAge}")
                 return invalidAge
-            
 
-        
-    
 
-    
-    
+
+
+
+
+
     # if input == integer
         # print as INT
         # continue
-    # else 
-        # print as non-INT 
+    # else
+        # print as non-INT
         # re-prompt user
-        
-        
-    # while (getAge != INT): 
+
+
+    # while (getAge != INT):
         #   repeat user input to be strictly whole numbers
         #   omit any other type of input (including ASCII characs)
-        
-    # return INT ONLY 
-    
-    
-    
-    
+
+    # return INT ONLY
+
+
+
+
 
 #     print(f"getAge_type:{type(getAge)}\n")
 #     validAge = True
 
-#     if getAge < 18: 
+#     if getAge < 18:
 #         validAge = False
 
 #         print("You are not eligible to drive yet!\n")
 #         return validAge
-    
+
 #     elif getAge >= 18:
 #         print("You are old enough to drive!\n")
 #         validAge = True
-    
-#     elif getAge is type(str):  
-    
+
+#     elif getAge is type(str):
+
 #         if validAge == True:
 #             return True
-    
-#     if validAge == False: 
+
+#     if validAge == False:
 #         print("Need a driving age!\n")
 #         return validAge
-    
+
 #     else:
 #         print("else condition: validAge == True")
-        
+
 
 
 def main():
